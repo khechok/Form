4c41b7ad3202f61089bac980301062f6076b88ca
Khechok <131937914+khechok@users.noreply.github.com>
2023-09-14 12:01:52 +0530

    Add files via upload

---
+++ Project-1/result.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+   const params = (new URL(document.locaation)).searchParmas;
+   const name = params.get('name');
+   const email = params.get('email');
+
+   document.getElementById
+}
+)
